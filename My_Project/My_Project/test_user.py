47902a8e9af3d9edf0f86da8201391ab02a8e6af
owenbob <obyomuhangi@gmail.com>
2017-08-16 16:50:08 +0300

    Commit to add to user.py and test_user.py the test for registerUser method

--- My_Project/My_Project/test_user.py
+++ My_Project/My_Project/test_user.py
@@ -7,4 +7,17 @@ class UserTestCase(unittest.TestCase):
 
     #Test is the user has Username Variable
     def test_userName(self):
-        self.assertEqual(self.my_login.userName,None,msg ="User has no Login")
+        self.assertEqual(self.my_login.userName,None,msg ="User has no Username")
+
+        #Test is the user has Username Variable
+    def test_emailaddress(self):
+        self.assertEqual(self.my_login.emailaddress,None,msg ="User has no email address")
+
+    #Test is the user has Username Variable
+    def test_password(self):
+        self.assertEqual(self.my_login.password,None,msg ="User has no Password")
+
+    #Test for add item to an empty list
+    def test_registerUser(self):
+        self.my_login.registerUser(["Joab","Joab@Andela.com","Joab123xyz"])
+        self.assertEqual(self.my_login.database, [["Joab","Joab@Andela.com","Joab123xyz"]], msg="User Not Registered")
--- My_Project/My_Project/user.py
+++ My_Project/My_Project/user.py
@@ -1,7 +1,12 @@
 class User(object):
     #Assigned three variables  of the user and initilized them to none
-     def __init__(self,userName=None,emailaddress=None,password=None):
+     def __init__(self,userName=None,emailaddress=None,password=None,database=[]):
          self.userName=userName
          self.emailaddress=emailaddress
          self.password=password
+         self.database = []
+     
+     def registerUser(self,list):
+        self.database.append(list)
+        return self.database
             
