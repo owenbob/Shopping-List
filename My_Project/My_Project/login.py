5f90bb367747c57fc28926d6ce34d97b800e8b2b
owenbob <obyomuhangi@gmail.com>
2017-08-16 18:11:23 +0300

    Commit to add test_login.py and login.py

---
+++ My_Project/My_Project/login.py
@@ -0,0 +1,6 @@
+class Login(object):
+    def __init__(self,UserName="Joab",Password="Joab@Andela.com"):
+        self.UserName=UserName
+        self.Password = Password
+    def loginUser(self,UserName,Password):
+            return("Welcome")
