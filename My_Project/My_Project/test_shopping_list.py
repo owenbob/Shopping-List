646de098d75f929b9001c941eaa00baae0619ec4
owenbob <obyomuhangi@gmail.com>
2017-08-16 15:14:46 +0300

    Commit to add unit tests for DELETE TestCase

--- My_Project/My_Project/test_shopping_list.py
+++ My_Project/My_Project/test_shopping_list.py
@@ -3,12 +3,22 @@ from shopping_list import ShoppingList
 
 class ShoppingListTestCase(unittest.TestCase):
     def setUp(self):
+        #test to see if List has been created
         self.mylist = ShoppingList()
-    def test_List(self):
-        self.assertTrue(self.mylist.lst,None,msg ="list not created"
+    def test_list(self):
+        self.assertEqual(self.mylist.lst,[],msg ="List has not been created")
+         #Test for add item to an empty list
     def test_add_to_list(self):
-        #Test for add item to an empty list
-        self.mylist.add_to_list('milk')
-        self.assertEqual(self.mylist.lst, ['milk'], msg="Item not Added")
+        self.mylist.add_to_list('wine')
+        self.assertEqual(self.mylist.lst, ['wine'], msg="List has not been pdated with new item")
+        #Test for updating list with a new item
+    def update_with_new_item(self):
+        self.mylist.add_to_list('blanket')
+        self.assertEqual(self.mylist.lst, ['wine'], msg="Item item has not been  removed")
+        #Test for removing an item from list
+    def delete_from_list(self):
+        self.mylist.delete_from_list("blanket")
+        self.assertEqual(self.mylist.lst, ['wine'], msg="Item not Added")
+    
                 
       
